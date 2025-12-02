ec145a255a7eae5594cdb6ab64dd4cf3a4be3dbe
Z <zrreid97@gmail.com>
2025-12-01 19:47:32 -0500

    Updated the test to check for the correct sample user.

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -19,8 +19,8 @@ class RegistrationTest extends TestCase
     public function test_new_users_can_register()
     {
         $response = $this->post(route('register.store'), [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Test Manager',
+            'email' => 'test_manager@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
         ]);
